16cfcbd765ad134b6f58b858c1ab95769aa2c2d5
Mateusz Gurgul <mateusz.gurgul.2003@gmail.com>
2026-03-26 10:42:09 +0100

    Fix iframe

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://siatki-kramer-generatorofert.onrender.com/',
         changeOrigin: true,
       }
     }
